e0920c12df73840531746f052009067b2a13ab95
Adelson Castillo <adelsonramon@gmail.com>
2024-09-15 13:16:27 -0500

    Finished second section of form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
   import {ref} from 'vue'
 
-
+  const myArray = ref([])
   const name = ref('')
-  
+  const input_content = ref('')
+  const input_category = ref(null)
+
+  const addTodo = () =>{
+    if(input_content.value.trim() === '' || input_category.value == null){
+      return
+    }
+    
+    myArray.value.push({
+      content: input_content.value,
+      category: input_category.value,
+    })
+
+    input_content.value = ''
+    input_category.value = null
+  }
+
 </script>
 
 <template>
@@ -18,8 +34,26 @@
     </section>
 
     <section class="create-todo">
+      <h3>CREATE A TO DO LIST</h3>
+      <form @submit.prevet = "addTodo">
+        <h4>What's on your to do list?</h4>
+        <input type="text" placeholder="e.g., Make a video" v-model="input_content"/>
 
-
+      <h4>Pick a category</h4>
+      <div class="options">
+        <label>
+          <input type="radio" name="category" value="business" v-model="input_category">
+          <span class="bubble business"></span>
+          <div>Business</div>
+        </label>
+        <label>
+          <input type="radio" name="category" value="Personal" v-model="input_category">
+          <span class="bubble Personal"></span>
+          <div>Personal</div>
+        </label>
+      </div>
+      <input type="submit" value="Add To Do"/>
+    </form>
     </section>
 
     <section class="todo-list">
